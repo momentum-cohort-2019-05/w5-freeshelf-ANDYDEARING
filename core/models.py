e16bcec32734a7622f6eaad22dba237838d9aa23
Andy Dearing <andy.dearing@gmail.com>
2019-06-25 08:26:54 -0400

    working book list view

--- core/models.py
+++ core/models.py
@@ -5,6 +5,10 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     url = models.URLField(unique=True)
     description = models.TextField(max_length=1000)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = [ 'added_at', 'title']
 
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from core.models import Book
 
 # Create your views here.
+class BookListView(generic.ListView):
+    model = Book
